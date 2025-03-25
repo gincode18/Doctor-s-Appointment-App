8e56af22d836b0867597a98514defe510c425c1f
gincode18 <vishalkamboj9211@gmail.com>
2025-03-25 15:58:39 +0530

    Update header component to replace 'Explore' menu item with 'AI Chat' and change logo image to a new JPEG format for improved visual consistency.

--- Doctor-Appointment-Booking-Web-Nextjs-main/app/_components/Header.jsx
+++ Doctor-Appointment-Booking-Web-Nextjs-main/app/_components/Header.jsx
@@ -20,8 +20,8 @@ function Header() {
         },
         {
             id:2,
-            name:'Explore',
-            path:'/explore'
+            name:'AI Chat',
+            path:'/ai-chat'
         },
         {
             id:3,
@@ -39,9 +39,9 @@ function Header() {
     <div className='flex items-center 
     justify-between p-4 shadow-sm'>
         <div className='flex items-center gap-10'>
-            {/* <Image src='/logo.svg' alt='logo'
-            width={180} height={80}
-            /> */}
+            <Image src='/logo.jpeg' alt='logo'
+            width={80} height={40}
+            />
             <ul className='md:flex gap-8 hidden'>
                 {Menu.map((item,index)=>(
                     <Link href={item.path} key={index}>
